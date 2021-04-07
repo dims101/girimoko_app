0299632a622031f09cc55108b5c1d6833df2d2a1
malsinur23 <malsinur.a.r@gmail.com>
2021-04-07 09:11:14 +0700

    tambah layout

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app', [
+  'activePage' => 'dashboard', 
+  'titlePage' => __('Dashboard')
+  ])
+
+  @section('content')        
+  <div class="content">
+  <div class="container-fluid">
+    <div class="card">
+  
+    <!-- <div class="card-body">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        <div class="containter">
+         Halaman di akses oleh (<span>  </span>) Departemen
+        </div>                    
+    </div> -->
+            <div class="card-header card-header-primary">
+              <h4 class="card-title">Girimoko</h4>
+              <p class="card-category">Data</p>
+            </div>
+            <div class="card-body table-responsive">
+              <table class="table table-hover">
+                <thead class="text-primary">
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Salary</th>
+                  <th>Country</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Dakota Rice</td>
+                    <td>$36,738</td>
+                    <td>Niger</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Minerva Hooper</td>
+                    <td>$23,789</td>
+                    <td>Curaçao</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Sage Rodriguez</td>
+                    <td>$56,142</td>
+                    <td>Netherlands</td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>Philip Chaney</td>
+                    <td>$38,735</td>
+                    <td>Korea, South</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div> 
+</div>  
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+    <div class="container">
+        <!-- <nav class="float-left">
+        <ul>
+            <li>
+            <a href="#">
+                {{ __('Creative Tim') }}
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                {{ __('About Us') }}
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                {{ __('Blog') }}
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                {{ __('Licenses') }}
+            </a>
+            </li>
+        </ul>
+        </nav> -->
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>
+        {{__(" Modified by")}}
+      <a href="#" target="_blank">{{__(" Dwi, Dimas & Malsi")}}</a>.
+      {{__(" Designed by")}}
+        <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+        </div>
+    </div>
+</footer>
