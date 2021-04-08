2b50216e1fba354a556d6c5698c8e7b207815884
dims101 <dimas.stmp@gmail.com>
2021-04-08 12:59:50 +0700

    Tambah login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,27 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index()
     {
         return view('dashboard');
     }
-
-    public function login()
+    public function login(Request $request)
     {
-        return view('login');
+        return json_encode($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/login', 'HomeController@login');
+Route::get('/dashboard', 'HomeController@index')->name('home');
